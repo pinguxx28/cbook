e42b65fd054ac49b273ff2f374ecbeaf039d6aa9
pinguxx28 <riskumax@gmail.com>
2023-04-30 17:02:13 +0300

    4.1

---
+++ src/4/4.1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define PATTERN "ould"
+
+int get_line(char line[], int max);
+int strindex(char str[], char pattern[]);
+
+int main()
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	while (get_line(line, MAXLINE) > 0)
+		if (strindex(line, PATTERN) >= 0) {
+			printf("%s", line);
+			found++;
+		}
+
+	return found;
+}
+
+int get_line(char line[], int max)
+{
+	int c, i;
+
+	i = 0;
+	while (--max > 0 && (c=getchar()) != EOF && c != '\n')
+		line[i++] = c;
+	if (c == '\n')
+		line[i++] = c;
+	line[i] = '\0';
+	return i;
+}
+
+int strindex(char str[], char pattern[])
+{
+	int i, j, k;
+
+	for (i = 0; str[i] != '\0'; i++) {
+		for (j=i, k=0; pattern[k] != '\0' && str[j]==pattern[k]; j++, k++) ;
+
+		if (k > 0 && pattern[k] == '\0')
+			return i;
+	}
+
+	return -1;
+}
