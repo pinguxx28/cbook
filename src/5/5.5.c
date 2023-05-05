1f9304f47c1f921309d691263e6adf92b73c0619
pinguxx28 <riskumax@gmail.com>
2023-05-05 06:48:36 +0300

    5.5

--- src/5/5.5.c
+++ src/5/5.5.c
@@ -5,10 +5,19 @@ void str_cpy(char *s, char *t)
 	while (*s++ = *t++);
 }
 
+int str_cmp(char *s, char *t)
+{
+	for (; *s == *t; s++, t++)
+		if (*s == '\0')
+			return 0;
+	return *s - *t;
+}
+
 int main()
 {
 	char a[] = "hello";
 	char b[] = "apple";
 	str_cpy(a, b);
 	printf("%s\n", a);
+	printf("%d\n", str_cmp(a, b));
 }
