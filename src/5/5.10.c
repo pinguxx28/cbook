9d4b624802dde0fbdc90a88a171763653461d274
pinguxx28 <riskumax@gmail.com>
2023-05-08 21:00:19 +0300

    5.10

---
+++ src/5/5.10.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int get_line(char *line, int max);
+
+/* find: print lines that match pattern from 1st arg */
+int main(int argc, char *argv[])
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	if (argc != 2)
+		printf("Usage: find pattern\n");
+	else
+		while (get_line(line, MAXLINE) > 0)
+			if (strstr(line, argv[1]) != NULL) {
+				printf("%s", line);
+				found++;
+			}
+	return found;
+}
+
+int get_line(char *line, int max)
+{
+	int c, i;
+
+	i = 0;
+	while (--max > 0 && (c = getchar()) != EOF && c != '\n')
+		line[i++] = c;
+	if (c == '\n')
+		line[i++] = c;
+	line[i] = '\0';
+	return i;
+}
